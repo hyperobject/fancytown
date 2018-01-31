01724cfa272f72adee7a2e63c7817e53398101f3
Connor Hudson <cohu3291@colorado.edu>
2018-01-31 15:43:57 -0700

    Created theme for Material UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#819ca9',
+      main: '#546e7a',
+      dark: '#29434e',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#9effff',
+      main: '#64ffda',
+      dark: '#14cba8',
+      contrastText: '#000',
+    },
+  },
+});
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
